881f6947b1ea82e81c514c27a0722f4fa60eeedc
Andriy Danylchenko <anry@zebrasus.com>
2021-05-27 13:13:40 +0300

    inline tailwind classes

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -4,9 +4,13 @@ document.getElementById('toggle-mobile-menu').addEventListener('click', function
     document.querySelector('.header nav').classList.toggle('open');
 });
 
-document.querySelectorAll('.dropdown a').forEach(function(a) {
+document.querySelectorAll('.dropdown > a').forEach(function(a) {
     a.addEventListener('click', function(e) {
         e.preventDefault();
+        
+        var open = e.target.closest('nav').querySelector('.open');
+        if(open && e.target.nextElementSibling != open) open.classList.remove('open');
+
         e.target.nextElementSibling.classList.toggle('open');
     });
 });
